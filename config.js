66067734d6e9fecaf19660f64cc1782139824c39
Sourabh Dharpure <35331618+sourabhdharpure@users.noreply.github.com>
2024-04-13 12:25:08 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   PORT: process.env.PORT || 3000,
-  PINECONE_API_KEY: process.env.PINECONE_API_KEY || "6896d757-b331-4412-a78a-10f16e3fd092",
-  PINECONE_INDEX_NAME: process.env.PINECONE_INDEX_NAME || "highlevel",
+  PINECONE_API_KEY: process.env.PINECONE_API_KEY ,
+  PINECONE_INDEX_NAME: process.env.PINECONE_INDEX_NAME ,
 };
